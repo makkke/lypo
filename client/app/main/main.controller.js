d4102cac8dbb0fdd84dd9a893252b1de84151328
Viacheslav Ivanov <slava.eth@gmail.com>
2015-03-16 22:40:36 -0700

    added inmobi adds

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -13,8 +13,16 @@
     activate();
 
     ////////////////////////////////
-    
+
     function activate() {
+      var headerAdConfig = {
+        siteid: 'e3ea0c5bd79945d6919e55459daad59e',
+        slot: '4',
+        manual: true,
+        test: true,
+        testdeviceid: '0001'
+      };
+      _inmobi.getNewAd(document.getElementById('header-ad'));
     }
   }
 })();
